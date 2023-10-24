43e31290ee01a99f267be481d94cc18bbaa988ee
HusseinHany <hhany3997@gmail.com>
2023-10-24 23:31:17 +0300

    Connect to database

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 
+// TODO connect to database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://3ein39:OL0CXpwFnerkEmaL@cluster0.xl4xaqc.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+mongoose.connection.on('open', () => {
+    console.log('Connected to MongoDB');
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 }
 );
-// TODO create database schemas
