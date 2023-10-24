43e690703fb8545c09b8543b269fac8322dd361e
HusseinHany <hhany3997@gmail.com>
2023-10-24 23:40:24 +0300

    Seed fake data

---
+++ seed.js
@@ -0,0 +1,49 @@
+// seeds file for fake data for testing purposes
+// run this file to seed the database with fake data
+
+const mongoose = require('mongoose');
+const Post = require('./models/post');
+const User = require('./models/user');
+const Comment = require('./models/comment');
+
+mongoose.connect('mongodb+srv://3ein39:OL0CXpwFnerkEmaL@cluster0.xl4xaqc.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+mongoose.connection.on('open', () => {
+console.log('Connected to MongoDB');
+}
+);
+
+// create a user
+const user = new User({
+    name: 'Hussein Hany',
+    email: 'hussein.hany.cs@gmail.com',
+    password: '123456'
+});
+
+// create a post
+const post = new Post({
+    title: 'Hello title',
+    content: 'Hello content',
+    date: new Date(),
+    published: true,
+    userID: user._id
+});
+
+// create a comment
+const comment = new Comment({
+    content: 'Hello comment',
+    date: new Date(),
+    userID: user._id,
+    postID: post._id
+});
+
+// save the user
+async function run() {
+    await user.save();
+    await post.save();
+    await comment.save();
+    console.log({user, post, comment});
+
+}
+
+run();
