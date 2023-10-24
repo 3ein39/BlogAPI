c8b4153d814f06d5e7eebda5962db03460a106e2
HusseinHany <hhany3997@gmail.com>
2023-10-24 23:20:28 +0300

    Fix model

--- models/post.js
+++ models/post.js
@@ -6,6 +6,7 @@ const postSchema = new Schema(
         title: String,
         content: String,
         date: Date,
+        published: Boolean,
         // link to user in user collection
         userID: { type: Schema.Types.ObjectId, ref: 'User' }
     });
